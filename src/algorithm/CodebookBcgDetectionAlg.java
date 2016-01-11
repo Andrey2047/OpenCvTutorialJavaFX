2fd0984c1d3c8b43f9df4468276584be9c6f9f62
andrey2047 <king_123@ukr.net>
2016-01-11 18:45:49 +0200

    fgdf

--- src/algorithm/CodebookBcgDetectionAlg.java
+++ src/algorithm/CodebookBcgDetectionAlg.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 import java.util.Date;
@@ -12,54 +13,82 @@ public class CodebookBcgDetectionAlg {
     private Codebook[][] codebook = new Codebook[640][480];
 
     public void updateCodebook(Mat frame) {
-
         for (int i = 0; i < frame.rows(); i++) {
             for (int j = 0; j < frame.cols(); j++) {
                 double[] currentPixel = frame.get(i, j);
                 Codebook pixelCodeBook = codebook[i][j];
-
                 for (int elemIndex = 0; elemIndex < pixelCodeBook.elementsCount; elemIndex++) {
-                    int matchedChannels = 0;
-                    for (int channel = 0; channel < 3; channel++) {
-
-                        if (pixelCodeBook.elements[elemIndex].minVal[channel] <= currentPixel[channel]
-                                && pixelCodeBook.elements[elemIndex].maxVal[channel] >= currentPixel[channel]) {
-                            matchedChannels++;
-                        }
-                    }
-                    if (matchedChannels == 3) {
-                        for (int channel = 0; channel < 3; channel++) {
-                            if (pixelCodeBook.elements[elemIndex].minVal[channel] >= currentPixel[channel]) {
-                                pixelCodeBook.elements[elemIndex].minVal[channel] = (int) currentPixel[channel];
-                            }
-
-                            if (pixelCodeBook.elements[elemIndex].maxVal[channel] <= currentPixel[channel]) {
-                                pixelCodeBook.elements[elemIndex].maxVal[channel] = (int) currentPixel[channel];
-                            }
-                        }
+                    if (matchPixel(currentPixel, pixelCodeBook, elemIndex)) {
+                        adjustThresholds(currentPixel, pixelCodeBook, elemIndex);
                     } else {
-                        pixelCodeBook.elementsCount++;
-                        CodebookElement codebookElement = new CodebookElement();
-                        for (int channel = 0; channel < 3; channel++) {
-                            codebookElement.maxVal[channel] = (int) currentPixel[channel];
-                            codebookElement.minVal[channel] = (int) currentPixel[channel];
-                        }
-                        codebookElement.t_last_update = pixelCodeBook.t;
-                        codebookElement.stale = 0;
-                        pixelCodeBook.elements[pixelCodeBook.elementsCount] = codebookElement;
+                        createNewBlob(currentPixel, pixelCodeBook);
                     }
                 }
             }
         }
     }
 
-    private int clearStaleEntries(Codebook[][] codebooks){
+    private void createNewBlob(double[] currentPixel, Codebook pixelCodeBook) {
+        pixelCodeBook.elementsCount++;
+        CodebookElement codebookElement = new CodebookElement();
+        for (int channel = 0; channel < 3; channel++) {
+            codebookElement.maxVal[channel] = (int) currentPixel[channel];
+            codebookElement.minVal[channel] = (int) currentPixel[channel];
+        }
+        codebookElement.t_last_update = pixelCodeBook.t;
+        codebookElement.stale = 0;
+        pixelCodeBook.elements[pixelCodeBook.elementsCount] = codebookElement;
+    }
+
+    private void adjustThresholds(double[] currentPixel, Codebook pixelCodeBook, int elemIndex) {
+        for (int channel = 0; channel < 3; channel++) {
+            if (pixelCodeBook.elements[elemIndex].minVal[channel] >= currentPixel[channel]) {
+                pixelCodeBook.elements[elemIndex].minVal[channel] = (int) currentPixel[channel];
+            }
+            if (pixelCodeBook.elements[elemIndex].maxVal[channel] <= currentPixel[channel]) {
+                pixelCodeBook.elements[elemIndex].maxVal[channel] = (int) currentPixel[channel];
+            }
+        }
+    }
+
+    private boolean matchPixel(double[] currentPixel, Codebook pixelCodeBook, int elemIndex) {
+        int matchedChannels = 0;
+        for (int channel = 0; channel < 3; channel++) {
+
+            if (pixelCodeBook.elements[elemIndex].minVal[channel] <= currentPixel[channel]
+                    && pixelCodeBook.elements[elemIndex].maxVal[channel] >= currentPixel[channel]) {
+                matchedChannels++;
+            }
+        }
+        return matchedChannels == 3;
+    }
+
+    public void clearStaleEntries(Codebook[][] codebooks){
         for (int i = 0; i < 640; i++) {
             for (int j = 0; j < 480; j++) {
                 Codebook codebook = codebooks[i][j];
-                i = filterStaleElements(codebook);
+                codebooks[i][j] = filterStaleElements(codebook);
             }
         }
+    }
+
+    public Mat backGroundDiff(Mat frame){
+        Mat toReturn = new Mat(frame.rows(), frame.cols(), CvType.CV_8UC1);
+        for (int i = 0; i < frame.rows(); i++) {
+            for (int j = 0; j < frame.cols(); j++) {
+                Codebook codebookForPixel = codebook[i][j];
+                double[] currentPixel = frame.get(i, j);
+                for (int elemIndex = 0; elemIndex < codebookForPixel.elementsCount; elemIndex++) {
+                    if(matchPixel(currentPixel, codebookForPixel, elemIndex)){
+                        toReturn.put(i,j, new int[]{255});
+                        break;
+                    } else {
+                        toReturn.put(i,j,new int[]{255});
+                    }
+                }
+            }
+         }
+        return toReturn;
     }
 
     private Codebook filterStaleElements(Codebook codebook) {
